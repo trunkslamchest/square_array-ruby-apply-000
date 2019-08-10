a1e16b71923159f09151f9984f215e315a0a880a
Austin Smith <trunk9slamchest@gmail.com>
2019-08-10 16:23:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,11 @@ def square_array(array)
   new_arr = []
   array.each do |n|
     new_arr << n * n
-    return new_arr
+    puts new_arr
   end
 end
 
-puts square_array([1,2,3])
-puts square_array([9,10,16,25])
+square_array([1,2,3])
+
+# puts square_array([1,2,3])
+# puts square_array([9,10,16,25])
